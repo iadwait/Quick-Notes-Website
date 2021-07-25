b202578cbca0e839258eaf3bc7bc43734e3be77d
iadwait <adwaitbarkale@gmail.com>
2021-07-25 17:24:55 +0530

    Worked on Adding User entered notes in notes array and setting it to local storage

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,26 @@
 console.log('Notes App');
+let objNotes = [];
+// Add Note Button
+let btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click', function (e) {
+    // console.log('Tapped');
+    // TextAre
+    let addNoteTxt = document.getElementById('txtNote');
+    // Retrive Notes Key Data from Local Storage
+    let notesData = localStorage.getItem('notes');
+    // Check if Notes are Null
+    if (notesData == null) {
+        // Initialize Notes Object
+        objNotes = [];
+    } else {
+        // Parse String to get Array
+        objNotes = JSON.parse(notesData);
+    }
+    // Add New Note Data in Notes Array
+    objNotes.push(addNoteTxt.value);
+    // Update Data in Local Storage
+    localStorage.setItem('notes',JSON.stringify(objNotes));
+    // Make Value of TextArea Empty because Note is Added
+    addNoteTxt.value = "";
+    console.log(objNotes);
+});
