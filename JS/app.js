598a532c9029165b09da6d00df0d287020f843e3
iadwait <adwaitbarkale@gmail.com>
2021-07-25 18:07:25 +0530

    Added Delete Note Functionality

--- JS/app.js
+++ JS/app.js
@@ -43,24 +43,44 @@ function showNotes() {
 
     let html = ``;
     // Create HTML - n Divs containing Notes
-    objNotes.forEach(function (element,index) {
+    objNotes.forEach(function (element, index) {
         html += `
         <div class="card my-3 mx-3 noteCard" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Note ${index + 1}</h5>
                     <p class="card-text">${element}</p>
-                    <a href="#" class="btn btn-primary">Delete note</a>
+                    <a id="${index}" class="btn btn-primary" onclick="deleteNote(this.id)">Delete note</a>
                 </div>
             </div>
         `
     })
     // Check if atleast 1 note is present or not and Render UI Respectively
     let displaynotesElement = document.getElementById('displayNotes');
-    if(objNotes.length != 0){
+    if (objNotes.length != 0) {
         // Display Notes
         displaynotesElement.innerHTML = html;
     } else {
         // Display No Notes Present
         displaynotesElement.innerHTML = `You have no notes, Notes you Add will be Displayed Here !!!!`;
     }
+}
+
+// Function Call to Delete a Note
+function deleteNote(index) {
+    console.log("Delete = " + index);
+    // Fetch Notes from Local Storage
+    let notesData = localStorage.getItem('notes');
+    // Check if Notes are Null
+    if (notesData == null) {
+        // Initialize Notes Object
+        objNotes = [];
+    } else {
+        // Parse String to get Array
+        objNotes = JSON.parse(notesData);
+    }
+    // Remove Note(index) from Array
+    objNotes.splice(index,1);
+    // Update Local Storage
+    localStorage.setItem('notes',JSON.stringify(objNotes));
+    showNotes();
 }
