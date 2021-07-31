ba18ad860c3b7fea9096b82006ff142a612a6b45
iadwait <adwaitbarkale@gmail.com>
2021-07-31 16:17:54 +0530

    Added Title Functionality

--- JS/app.js
+++ JS/app.js
@@ -6,23 +6,36 @@ showNotes()
 let btnAdd = document.getElementById('btnAdd');
 btnAdd.addEventListener('click', function (e) {
     // console.log('Tapped');
+    // Text Title
+    let addNoteTitle = document.getElementById('txtTitle');
     // TextAre
     let addNoteTxt = document.getElementById('txtNote');
+    // Retrive Notes Title Key Data from Local Storage
+    let notesTitle = localStorage.getItem('title');
     // Retrive Notes Key Data from Local Storage
     let notesData = localStorage.getItem('notes');
     // Check if Notes are Null
     if (notesData == null) {
         // Initialize Notes Object
+        objTitle = [];
         objNotes = [];
     } else {
         // Parse String to get Array
+        objTitle = JSON.parse(notesTitle);
         objNotes = JSON.parse(notesData);
     }
     // Add New Note Data in Notes Array
+    if (addNoteTitle.value.length != 0) {
+        objTitle.push(addNoteTitle.value);
+    } else {
+        objTitle.push(`Note ${objTitle.length + 1}`);
+    }
     objNotes.push(addNoteTxt.value);
     // Update Data in Local Storage
+    localStorage.setItem('title',JSON.stringify(objTitle));
     localStorage.setItem('notes', JSON.stringify(objNotes));
     // Make Value of TextArea Empty because Note is Added
+    addNoteTitle.value = "";
     addNoteTxt.value = "";
     console.log(objNotes);
     showNotes();
@@ -32,6 +45,7 @@ btnAdd.addEventListener('click', function (e) {
 function showNotes() {
     // Get All Notes From Local Storage
     let notesData = localStorage.getItem('notes');
+    let notesTitle = localStorage.getItem('title');
     // Check if Notes are Null
     if (notesData == null) {
         // Initialize Notes Object
@@ -41,13 +55,15 @@ function showNotes() {
         objNotes = JSON.parse(notesData);
     }
 
+    objTitle = JSON.parse(notesTitle);
+
     let html = ``;
     // Create HTML - n Divs containing Notes
     objNotes.forEach(function (element, index) {
         html += `
         <div class="card my-3 mx-3 noteCard" style="width: 18rem;">
                 <div class="card-body">
-                    <h5 class="card-title">Note ${index + 1}</h5>
+                    <h5 class="card-title">${objTitle[index]}</h5>
                     <p class="card-text">${element}</p>
                     <a id="${index}" class="btn btn-primary" onclick="deleteNote(this.id)">Delete note</a>
                 </div>
@@ -70,18 +86,23 @@ function deleteNote(index) {
     console.log("Delete = " + index);
     // Fetch Notes from Local Storage
     let notesData = localStorage.getItem('notes');
+    let notesTitle = localStorage.getItem('title');
     // Check if Notes are Null
     if (notesData == null) {
         // Initialize Notes Object
         objNotes = [];
+        objTitle = [];
     } else {
         // Parse String to get Array
         objNotes = JSON.parse(notesData);
+        objTitle = JSON.parse(notesTitle);
     }
     // Remove Note(index) from Array
     objNotes.splice(index,1);
+    objTitle.splice(index,1);
     // Update Local Storage
     localStorage.setItem('notes',JSON.stringify(objNotes));
+    localStorage.setItem('title',JSON.stringify(objTitle));
     showNotes();
 }
 
